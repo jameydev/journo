18e196179e72881c33f4fd31457478c5897968f5
jameydev <jybryce4@gmail.com>
2023-12-28 13:07:52 -0600

    brought in code from mern-auth for handling user auth

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+
+// Get database and server configured
+dotenv.config();
+connectDB();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes);
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`.green.bold);
 });
