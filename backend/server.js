354c685c68da1ae7ea8225fe49af8c2a40f2a34c
jameydev <jybryce4@gmail.com>
2023-12-29 05:09:02 -0600

    added default get /

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
 app.use('/api/users', userRoutes);
 
 app.listen(port, () => {
