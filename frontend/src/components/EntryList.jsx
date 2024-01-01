3474fe95de1f0b430c2a57f830ba7cfcb47b8ea1
jameydev <jybryce4@gmail.com>
2024-01-01 17:46:53 -0600

    proptypes, _id prop, entries array

--- frontend/src/components/EntryList.jsx
+++ frontend/src/components/EntryList.jsx
@@ -1,14 +1,19 @@
+import PropTypes from 'prop-types';
 import Entry from './Entry';
 
-export default function EntryList() {
+export default function EntryList({ entries: [...entries]}) {
     
     return (
         <>
             {entries.map(entry => (
-                <Entry key={entry._id} title={entry.title}>
+                <Entry key={entry._id} _id={entry._id} title={entry.title}>
                     {entry.content}
                 </Entry>
-            ))};
+            ))}
         </>
     );
 }
+
+EntryList.propTypes = {
+    entries: PropTypes.array.isRequired
+};
