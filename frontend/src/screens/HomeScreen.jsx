a17631e28dc8388a050a5644678d66848a426a90
jameydev <jybryce4@gmail.com>
2024-01-01 15:52:03 -0600

    got rid of login and register Hero when logged in

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux';
 import Hero from '../components/Hero';
-import NewEntryScreen from './NewEntryScreen';
+// import NewEntryScreen from './NewEntryScreen';
+// import EntryList from '../components/EntryList';
+// import { useGetEntriesQuery } from '../slices/entriesApiSlice';
 
 export default function HomeScreen() {
     const { userInfo } = useSelector(state => state.auth);
 
+    // const { data, isLoading } = useGetEntriesQuery();
+
     return (
         <>
-            {userInfo ? <NewEntryScreen /> : <Hero />}
+            {userInfo ? 
+                (
+                    <>
+                        <h1 className="text-center mt-5">
+                            Welcome {userInfo.name}!
+                        </h1>
+                    </>
+                ):
+                <Hero />
+            }
         </>
     );
 }
