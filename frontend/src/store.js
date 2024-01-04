2627b8339b63e22c45559b3e4c043a9ccd9583b7
jameydev <jybryce4@gmail.com>
2024-01-04 05:29:35 -0600

    formatting fix

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import { apiSlice } from './slices/apiSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './slices/authSlice'
+import entriesReducer from './slices/entriesSlice'
+import { apiSlice } from './slices/apiSlice'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         [apiSlice.reducerPath]: apiSlice.reducer,
+        entries: entriesReducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
 });
 
