57791bb614471b6295dd4e487f8a310eace28884
jameydev <jybryce4@gmail.com>
2024-01-04 05:27:22 -0600

    fixed formatting

--- frontend/src/screens/NewEntryScreen.jsx
+++ frontend/src/screens/NewEntryScreen.jsx
@@ -7,7 +7,7 @@ import { toast } from 'react-toastify';
 import FormContainer from '../components/FormContainer';
 import Loader from '../components/Loader';
 import { useAddEntryMutation } from '../slices/entriesApiSlice';
-import { addEntry } from '../slices/entrySlice';
+import { addEntry } from '../slices/entriesSlice';
 
 export default function NewEntryScreen() {
     const [title, setTitle] = useState('');
@@ -28,56 +28,55 @@ export default function NewEntryScreen() {
 
     const submitHandler = async (e) => {
         e.preventDefault();
-
         if (!title || !content) {
             toast.error('Please title your entry and/or add some content');
-        }
+        } 
         else {
-            try {
-                const res = await entry({ title, content }).unwrap();
-                dispatch(addEntry({ ...res }));
-                toast.success('Entry added');
-                // navigate(`/journal/${res._id}`);
-            }
-            catch (err) {
-                toast.error(err?.data?.message || err.error);
-            }
+        try {
+            const res = await entry({ title, content }).unwrap();
+            dispatch(addEntry({ ...res }));
+            toast.success('Entry added');
+            // navigate(`/journal/${res._id}`);
+        } 
+        catch (err) {
+            toast.error(err?.data?.message || err.error);   
         }
-    };
+    }
+}
 
     return (
         <>
-            <FormContainer>
-                <h1 className='text-center'>New Entry</h1>
+        <FormContainer>
+            <h1 className="text-center">New Entry</h1>
 
-                <Form onSubmit={submitHandler}>
-                    <Form.Group className="my-2" controlId="title">
-                        <Form.Label>Title</Form.Label>
+            <Form onSubmit={submitHandler}>
+                <Form.Group className="my-2" controlId="title">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Entry Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                    />
+                </Form.Group>
+
+                <Form.Group className="my-2" controlId="content">
+                    <Form.Label>Content</Form.Label>
                         <Form.Control
-                            type='text'
-                            placeholder='Entry Title'
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            required
-                        />
-                    </Form.Group>
+                        as="textarea"
+                        placeholder="What do you want to write about?"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        required
+                    />
+                </Form.Group>
 
-                    <Form.Group className="my-2" controlId="content">
-                        <Form.Label>Content</Form.Label>
-                        <Form.Control
-                            as='textarea'
-                            placeholder='What do you want to write about?'
-                            value={content}
-                            onChange={(e) => setContent(e.target.value)}
-                            required
-                        />
-                    </Form.Group>
-
-                    <Button type='submit' variant='primary'>
-                        Save Entry
-                    </Button>
-                </Form>
-            </FormContainer>
+                <Button type="submit" variant="primary">
+                    Save Entry
+                </Button>
+            </Form>
+        </FormContainer>
         </>
     );
 }
