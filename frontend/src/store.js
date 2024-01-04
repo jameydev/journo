8a8fafa9ce37b7089c531d1d8424a6f4b8cf4ace
jameydev <jybryce4@gmail.com>
2024-01-04 05:31:56 -0600

    formatter kept removing semi-colons

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from './slices/authSlice'
-import entriesReducer from './slices/entriesSlice'
-import { apiSlice } from './slices/apiSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import entriesReducer from './slices/entriesSlice';
+import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
     reducer: {
