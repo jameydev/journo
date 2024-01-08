351bc4fee72a9289be9ea8fa8ff8fea9a45531a5
jameydev <jybryce4@gmail.com>
2024-01-08 14:02:32 -0600

    isSingle prop added to Entry component; if it is on the single page of that entry, it will show a back button (pass true in this case - otherwise, defaults to false)

--- frontend/src/components/Entry.jsx
+++ frontend/src/components/Entry.jsx
@@ -1,7 +1,7 @@
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export default function Entry({ title, children, _id }) {
+export default function Entry({ title, children, _id, isSingle = false }) {
     if (!title) {
         title = 'Untitled';
     }
@@ -11,25 +11,32 @@ export default function Entry({ title, children, _id }) {
     }
 
     return (
-                <>
-                    <div className="card mt-3">
-                        <div className="card-title mt-2 mx-2">
-                            <h3>{title}</h3>
-                            <Link to={`/journal/${_id}`}>View</Link>
-                        </div>
-                        <div className="card-body">
-                            <p>{children}</p>
-                        </div>
-                        <div className="card-footer">
-                            <button className="btn btn-primary mx-2">Edit</button>
-                            <button className="btn btn-danger mx-2">Delete</button>
-                        </div>
-                    </div>
-                </>
+        <>
+            <div className='card mt-3'>
+                <div className='card-title mt-2 mx-2'>
+                    <h3>{title}</h3>
+                    <Link to={`/journal/${_id}`}>View</Link>
+                </div>
+                <div className='card-body'>
+                    <p>{children}</p>
+                </div>
+                <div className='card-footer'>
+                    {isSingle && (
+                        <Link to='/'>
+                            <button className='btn btn-info mx-4'>Back</button>
+                        </Link>
+                    )}
+                    <button className='btn btn-primary mx-2'>Edit</button>
+                    <button className='btn btn-danger mx-2'>Delete</button>
+                </div>
+            </div>
+        </>
     );
 }
 
 Entry.propTypes = {
     title: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
-}
+    _id: PropTypes.string.isRequired,
+    isSingle: PropTypes.bool,
+};
